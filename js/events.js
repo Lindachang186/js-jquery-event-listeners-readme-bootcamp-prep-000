61fab40766c59fde32e640c57036675845c4c57b
Linda Chang <lindachang186@gmail.com>
2018-07-26 14:14:41 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -11,9 +11,9 @@ function frameIt(){
 }
 
 function pressIt(){
-  $('document').on('keydown', function(key) {
-    if(key.which == 71).val() === 'correct' {
-        alert('You have pressed G')
+  $(document).on('keydown', function(key){
+    if(key.which == 71) {
+      alert('G was pressed')
     }
   })
 }

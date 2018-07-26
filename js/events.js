5e0ac262582086e24a8415988cf6651bdcba31fe
Linda Chang <lindachang186@gmail.com>
2018-07-26 13:50:54 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,11 +1,10 @@
 function getIt() {
-  $('p').on("click", function()) {
+  $('p').on("click", function() {
     alert('Hey!')
     }
-}
+})
 
 $(document).ready(function(){
-  getit()
 // call functions here
 
 });
